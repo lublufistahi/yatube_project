919f7ed313ed0969f4d55db19f11cfa07d9f33d9
Андрей Серёгин <andrew.seriogin11@yandex.ru>
2022-11-30 23:39:10 +0300

    Работа с моделями

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -2,7 +2,9 @@ from django.urls import path
 
 from . import views
 
+app_name = 'posts'
+
 urlpatterns = [
-    path('', views.index),
+    path('', views.index, name='main_page'),
     path('group/<slug:slug>/', views.group_posts),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+from .models import Post, Group
+
 
 # Create your views here.
-def index(request):
-    return HttpResponse('Главная страница')
-
 def group_posts(request, slug):
-    return HttpResponse(f'Группа : {slug}')
+    template = "posts/group_list.html"
+    title = 'Здесь будет информация о группах проекта Yatube'
+    context = {'title':title}
+    return render(request, template, context)
+
+def index(request):
+    posts = Post.objects.order_by('-pub_date')[:10]
+    title = 'Это главная страница проекта Yatube'
+    context = {'posts': posts,
+               'title':title}
+    return render(request, 'posts/index.html', context) 
